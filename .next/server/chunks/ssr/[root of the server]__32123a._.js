f5a1507634482175950530febf85da9b9b1766da
Elmata2 <643689jw@eur.nl>
2024-12-09 20:28:46 +0000

    change of the content structure

--- .next/server/chunks/ssr/[root of the server]__32123a._.js
+++ .next/server/chunks/ssr/[root of the server]__32123a._.js
@@ -30,86 +30,23 @@ var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$gray$2d$matt
 ;
 ;
 ;
-const contentDirectory = __TURBOPACK__imported__module__$5b$externals$5d2f$__$5b$external$5d$__$28$path$2c$__cjs$29$__["default"].join(process.cwd(), 'src/content');
-function getMarkdownContent(filename) {
-    try {
-        const filePath = __TURBOPACK__imported__module__$5b$externals$5d2f$__$5b$external$5d$__$28$path$2c$__cjs$29$__["default"].join(contentDirectory, `${filename}.md`);
-        // Check if file exists
-        if (!__TURBOPACK__imported__module__$5b$externals$5d2f$__$5b$external$5d$__$28$fs$2c$__cjs$29$__["default"].existsSync(filePath)) {
-            throw new Error(`File ${filename}.md not found in content directory`);
-        }
-        const fileContents = __TURBOPACK__imported__module__$5b$externals$5d2f$__$5b$external$5d$__$28$fs$2c$__cjs$29$__["default"].readFileSync(filePath, 'utf8');
-        const { data, content } = (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$gray$2d$matter$2f$index$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["default"])(fileContents);
-        return {
-            metadata: data,
-            content: content
-        };
-    } catch (error) {
-        console.error(`Error reading markdown file ${filename}.md:`, error);
-        throw error;
-    }
+function getMarkdownContent(fileName) {
+    const filePath = __TURBOPACK__imported__module__$5b$externals$5d2f$__$5b$external$5d$__$28$path$2c$__cjs$29$__["default"].join(process.cwd(), 'content', `${fileName}.md`);
+    const fileContents = __TURBOPACK__imported__module__$5b$externals$5d2f$__$5b$external$5d$__$28$fs$2c$__cjs$29$__["default"].readFileSync(filePath, 'utf8');
+    const { data: metadata, content } = (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$gray$2d$matter$2f$index$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["default"])(fileContents);
+    return {
+        metadata,
+        content
+    };
 }
 }}),
-"[project]/src/app/mijn-verhaal/page.tsx [app-rsc] (ecmascript)": ((__turbopack_context__) => {
-"use strict";
+"[project]/src/app/mijn-verhaal/page.tsx [app-rsc] (ecmascript)": (function(__turbopack_context__) {
 
-var { r: __turbopack_require__, f: __turbopack_module_context__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, n: __turbopack_export_namespace__, c: __turbopack_cache__, M: __turbopack_modules__, l: __turbopack_load__, j: __turbopack_dynamic__, P: __turbopack_resolve_absolute_path__, U: __turbopack_relative_url__, R: __turbopack_resolve_module_id_path__, b: __turbopack_worker_blob_url__, g: global, __dirname, x: __turbopack_external_require__, y: __turbopack_external_import__, z: require } = __turbopack_context__;
+var { r: __turbopack_require__, f: __turbopack_module_context__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, n: __turbopack_export_namespace__, c: __turbopack_cache__, M: __turbopack_modules__, l: __turbopack_load__, j: __turbopack_dynamic__, P: __turbopack_resolve_absolute_path__, U: __turbopack_relative_url__, R: __turbopack_resolve_module_id_path__, b: __turbopack_worker_blob_url__, g: global, __dirname, x: __turbopack_external_require__, y: __turbopack_external_import__, m: module, e: exports, t: require } = __turbopack_context__;
 {
-__turbopack_esm__({
-    "default": (()=>VerhaalPage)
-});
-var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__ = __turbopack_import__("[project]/node_modules/next/dist/server/route-modules/app-page/vendored/rsc/react-jsx-dev-runtime.js [app-rsc] (ecmascript)");
-var __TURBOPACK__imported__module__$5b$project$5d2f$src$2f$lib$2f$markdown$2e$ts__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__ = __turbopack_import__("[project]/src/lib/markdown.ts [app-rsc] (ecmascript)");
-var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$markdown$2d$to$2d$jsx$2f$dist$2f$index$2e$module$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__ = __turbopack_import__("[project]/node_modules/markdown-to-jsx/dist/index.module.js [app-rsc] (ecmascript)");
-;
-;
-;
-function VerhaalPage() {
-    const { content, metadata } = (0, __TURBOPACK__imported__module__$5b$project$5d2f$src$2f$lib$2f$markdown$2e$ts__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["getMarkdownContent"])("jaap-wiebrens");
-    return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
-        className: "min-h-screen bg-black",
-        children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("main", {
-            className: "container mx-auto px-4 py-8",
-            children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("article", {
-                className: "prose prose-invert mx-auto",
-                children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$markdown$2d$to$2d$jsx$2f$dist$2f$index$2e$module$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["default"], {
-                    options: {
-                        overrides: {
-                            p: {
-                                component: ({ children, ...props })=>/*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
-                                        ...props,
-                                        children: children
-                                    }, void 0, false, {
-                                        fileName: "[project]/src/app/mijn-verhaal/page.tsx",
-                                        lineNumber: 16,
-                                        columnNumber: 21
-                                    }, void 0)
-                            }
-                        }
-                    },
-                    children: content
-                }, void 0, false, {
-                    fileName: "[project]/src/app/mijn-verhaal/page.tsx",
-                    lineNumber: 11,
-                    columnNumber: 11
-                }, this)
-            }, void 0, false, {
-                fileName: "[project]/src/app/mijn-verhaal/page.tsx",
-                lineNumber: 10,
-                columnNumber: 9
-            }, this)
-        }, void 0, false, {
-            fileName: "[project]/src/app/mijn-verhaal/page.tsx",
-            lineNumber: 9,
-            columnNumber: 7
-        }, this)
-    }, void 0, false, {
-        fileName: "[project]/src/app/mijn-verhaal/page.tsx",
-        lineNumber: 8,
-        columnNumber: 5
-    }, this);
-}
-}}),
+const e = new Error("Could not parse module '[project]/src/app/mijn-verhaal/page.tsx'");
+e.code = 'MODULE_UNPARSEABLE';
+throw e;}}),
 "[project]/src/app/mijn-verhaal/page.tsx [app-rsc] (ecmascript, Next.js server component)": ((__turbopack_context__) => {
 
 var { r: __turbopack_require__, f: __turbopack_module_context__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, n: __turbopack_export_namespace__, c: __turbopack_cache__, M: __turbopack_modules__, l: __turbopack_load__, j: __turbopack_dynamic__, P: __turbopack_resolve_absolute_path__, U: __turbopack_relative_url__, R: __turbopack_resolve_module_id_path__, b: __turbopack_worker_blob_url__, g: global, __dirname, t: require } = __turbopack_context__;
--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -5,23 +5,12 @@ import matter from 'gray-matter';
 const contentDirectory = path.join(process.cwd(), 'src/content');
 
 export function getMarkdownContent(filename: string) {
-  try {
-    const filePath = path.join(contentDirectory, `${filename}.md`);
-    
-    // Check if file exists
-    if (!fs.existsSync(filePath)) {
-      throw new Error(`File ${filename}.md not found in content directory`);
-    }
-    
-    const fileContents = fs.readFileSync(filePath, 'utf8');
-    const { data, content } = matter(fileContents);
-    
-    return {
-      metadata: data,
-      content: content
-    };
-  } catch (error) {
-    console.error(`Error reading markdown file ${filename}.md:`, error);
-    throw error;
-  }
+  const filePath = path.join(contentDirectory, `${filename}.md`);
+  const fileContents = fs.readFileSync(filePath, 'utf8');
+  const { data, content } = matter(fileContents);
+  
+  return {
+    metadata: data,
+    content: content
+  };
 }
